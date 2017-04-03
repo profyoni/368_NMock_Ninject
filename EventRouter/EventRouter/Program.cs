89bf98f9e8e2a530128877c1d6b8c1123a477ef7
profyoni <jrobinson@touro.edu>
2017-04-02 21:08:06 -0400

    g

--- EventRouter/EventRouter/Program.cs
+++ EventRouter/EventRouter/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace EventRouter
 {
     public class SystemTimer : ITimer
